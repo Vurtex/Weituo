05bb5f88d7ebe2a70bb6e12583809cd3c33611f5
Vurtex <hongkeshu@gmail.com>
2017-08-02 22:33:07 +0800

    Fix
    Avatar

---
+++ app/src/main/java/com/vurtex/weituo/entity/UploadResulte.java
@@ -0,0 +1,9 @@
+package com.vurtex.weituo.entity;
+
+/**
+ * Created by Vurtex on 2017/8/2.
+ */
+
+public class UploadResulte extends ResultInfo {
+
+}
--- app/src/main/java/com/vurtex/weituo/server/HttpServiceApi.java
+++ app/src/main/java/com/vurtex/weituo/server/HttpServiceApi.java
@@ -2,10 +2,15 @@ package com.vurtex.weituo.server;
 
 
 import com.vurtex.weituo.entity.LoginResult;
+import com.vurtex.weituo.entity.ResultInfo;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 import rx.Observable;
 
 /**
@@ -19,4 +24,8 @@ public interface HttpServiceApi {
     @FormUrlEncoded
     @POST("user/registerUser")
     Observable<LoginResult> doregister(@Field("username") String username, @Field("password") String password, @Field("nickname") String nickname, @Field("age") String age, @Field("avatar") String avatar);
+    /*上传文件*/
+    @Multipart
+        @POST("file/uploadAvatar")
+    Observable<ResultInfo> uploadImage(@Part("username") RequestBody username, @Part("auth_key") RequestBody  auth_key, @Part MultipartBody.Part file);
 }
