38b6e6ebff68b5ba6f39d9bb21e6ae73adcc3bf8
Vurtex <hongkeshu@gmail.com>
2017-10-20 23:51:39 +0800

    Fix

--- app/src/main/java/com/vurtex/weituo/activity/RegisterActivity.java
+++ app/src/main/java/com/vurtex/weituo/activity/RegisterActivity.java
@@ -84,6 +84,11 @@ public class RegisterActivity extends ImmersionBaseActivity implements LoaderCal
 
 
     @Override
+    protected int setLayoutId() {
+        return R.layout.activity_register;
+    }
+
+    @Override
     protected void initView() {
         populateAutoComplete();
         et_Password.setOnEditorActionListener((textView, id, keyEvent) -> {
@@ -97,11 +102,6 @@ public class RegisterActivity extends ImmersionBaseActivity implements LoaderCal
         btn_Submit.setOnClickListener((view) -> {
             attemptRegister();
         });
-    }
-
-    @Override
-    protected int setLayoutId() {
-        return R.layout.activity_register;
     }
 
     @Override
