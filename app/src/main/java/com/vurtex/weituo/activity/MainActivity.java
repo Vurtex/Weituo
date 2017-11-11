7c54940cbda2d9ccad5ff291e450520b2b7883c8
Vurtex <hongkeshu@gmail.com>
2017-11-11 23:39:18 +0800

    Fix

--- app/src/main/java/com/vurtex/weituo/activity/MainActivity.java
+++ app/src/main/java/com/vurtex/weituo/activity/MainActivity.java
@@ -65,7 +65,6 @@ public class MainActivity extends ImmersionBaseActivity {
         ButterKnife.bind(this);
     }
 
-
     public void initFragments() {
         mFragments = new ArrayList<>();
         mFragments.add(OneFragment.newInstance(0));
