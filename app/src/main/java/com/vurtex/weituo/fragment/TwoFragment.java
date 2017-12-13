c13ab117e2699df4c7b78b332bbfe2a59e8a8d2c
Vurtex <hongkeshu@gmail.com>
2017-12-13 22:11:32 +0800

    Fix

--- app/src/main/java/com/vurtex/weituo/fragment/TwoFragment.java
+++ app/src/main/java/com/vurtex/weituo/fragment/TwoFragment.java
@@ -25,7 +25,6 @@ import java.util.ArrayList;
 
 import butterknife.BindView;
 
-
 public class TwoFragment extends ImmersionBaseFragment implements SwipeRefreshLayout.OnRefreshListener,
         EasyViewHolder.OnItemClickListener {
 
